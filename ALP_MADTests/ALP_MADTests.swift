6441cd2e87d270de2f24210547fea0d898a78e3e
Nicho <nicholasleroy83@gmail.com>
2026-05-29 01:18:05 +0700

    add testing

---
+++ ALP_MADTests/ALP_MADTests.swift
@@ -0,0 +1,18 @@
+//
+//  ALP_MADTests.swift
+//  ALP_MADTests
+//
+//  Created by Nicholas Leroy kurniawan on 29/05/26.
+//
+
+import Testing
+
+struct ALP_MADTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
